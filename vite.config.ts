bdf51d4e5b8f586d6b6d466fa886591edb382f30
omkar kadam <103347564+Omkar0070@users.noreply.github.com>
2026-01-11 23:06:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+  plugins: [react(), tsconfigPaths()]
 })
